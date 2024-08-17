a7f2578bb7dcdff204beaa41dd7b86608d914243
Felipe Morilho <felipemdecastro@gmail.com>
2024-08-17 10:26:34 -0300

    feat: change category require to false

--- src/models/Product.js
+++ src/models/Product.js
@@ -7,7 +7,7 @@ const productSchema = new mongoose.Schema({
     name: { type: String, required: true },
     image: { type: String, required: true },
     price: { type: Number, required: true, min: [0.01, 'O Preço deve ser maior que zero!'] },
-    category: { type: String, required: true, enum: categoriesEnum },
+    category: { type: String, required: false, enum: categoriesEnum },
     section: { type: String, required: true, enum: sectionEnum }
 }, { versionKey: false });
 
